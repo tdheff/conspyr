a4a38ee203757e48397faf317ee242aac22178b5
Jared Pochtar <jpochtar@gmail.com>
2014-02-28 12:43:30 -0500

    excised login stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,19 +7,12 @@ angular.module('bookClubApp', [
   'ngRoute',
   'firebase',
   'angularfire.firebase',
-  'angularfire.login',
-  'simpleLoginTools'
 ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
-      .when('/login', {
-        authRequired: false, // if true, must log in before viewing this page
-        templateUrl: 'views/login.html',
-        controller: 'LoginController'
       })
       .otherwise({
         redirectTo: '/'
