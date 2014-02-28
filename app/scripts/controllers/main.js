c335db7a056220ffd55322a61f80b5881717a5a2
Jared Pochtar <jpochtar@gmail.com>
2014-02-28 10:55:56 -0500

    cleaning up for dev

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('bookClubApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $firebase, FBURL) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.books = new $firebase(new Firebase(FBURL + '/books'));
   });
