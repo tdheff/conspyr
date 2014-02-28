97f34d6c9b2223757044dac5b72e8fcbbeef717d
Jared Pochtar <jpochtar@gmail.com>
2014-02-28 11:37:14 -0500

    added book adding

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,17 @@
 angular.module('bookClubApp')
   .controller('MainCtrl', function ($scope, $firebase, FBURL) {
     $scope.books = new $firebase(new Firebase(FBURL + '/books'));
+
+    $scope.addBook = function() {
+    	$scope.books.$add({
+    		title: $scope.newbook_title,
+    		author: $scope.newbook_author,
+    		cover: $scope.newbook_cover,
+    	})
+
+    	$scope.newbook_title = '';
+    	$scope.newbook_author = '';
+    	$scope.newbook_cover = '';
+    };
+
   });
