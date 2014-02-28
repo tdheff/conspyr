dde32aed0bab3e87532e2c4d00d145e66609c60a
Jared Pochtar <jpochtar@gmail.com>
2014-02-28 11:13:02 -0500

    removed boilerplate

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,5 @@
 
 angular.module('bookClubApp')
   .controller('MainCtrl', function ($scope, $firebase, FBURL) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
     $scope.books = new $firebase(new Firebase(FBURL + '/books'));
   });
