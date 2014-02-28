c3a9d8e0344d72329fbe1c9425b795f1fb40ccb4
Jared Pochtar <jpochtar@gmail.com>
2014-02-28 12:25:20 -0500

    better book adding

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,13 +2,31 @@
 
 angular.module('bookClubApp')
   .controller('MainCtrl', function ($scope, $firebase, FBURL) {
+
+	var clean_str = function(str) {
+		if (str == null) {
+			return '';
+		}
+
+		return str.trim();
+	}
+
     $scope.books = new $firebase(new Firebase(FBURL + '/books'));
 
     $scope.addBook = function() {
+    	var title = clean_str($scope.newbook_title);
+    	var author = clean_str($scope.newbook_author);
+    	var cover = clean_str($scope.newbook_cover);
+
+    	if (title == '') {
+    		// don't add books with no title
+    		return;
+    	}
+
     	$scope.books.$add({
-    		title: $scope.newbook_title.trim(),
-    		author: $scope.newbook_author.trim(),
-    		cover: $scope.newbook_cover.trim(),
+    		title: title,
+    		author: author,
+    		cover: cover
     	})
 
     	$scope.newbook_title = '';
